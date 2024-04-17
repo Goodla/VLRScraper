59c48d34411afd6c42d4b86fd039512672fb0c63
Goodla <45775313+Goodla@users.noreply.github.com>
2024-04-17 02:04:44 -0700

    Create main.py

---
+++ utils/main.py
@@ -0,0 +1,38 @@
+import uvicorn
+from fastapi import FastAPI, Request
+
+from api.scraper import Vlr
+from slowapi import Limiter, _rate_limit_exceeded_handler
+from slowapi.errors import RateLimitExceeded
+from slowapi.util import get_remote_address
+
+limiter = Limiter(key_func=get_remote_address)
+
+app = FastAPI(
+    title="vlrggapi",
+    description="An Unofficial REST API for [vlr.gg](https://www.vlr.gg/), a site for Valorant Esports match and news "
+    "coverage. ",
+    docs_url="/",
+    redoc_url=None,
+)
+vlr = Vlr()
+
+TEN_MINUTES = 600
+
+app.state.limiter = limiter
+app.add_exception_handler(RateLimitExceeded, _rate_limit_exceeded_handler)
+
+
+@app.get("/news")
+@limiter.limit("250/minute")
+async def VLR_news(request: Request):
+    return vlr.vlr_recent()
+
+
+@app.get("/health")
+def health():
+    return "Healthy: OK"
+
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0", port=3001)
